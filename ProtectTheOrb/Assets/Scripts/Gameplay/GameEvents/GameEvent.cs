e82e48a4480985d8fdc9c0b6b0572867e49607e7
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-04-12 16:47:41 -0400

    Revert "add scriptable objects architecture"
    This reverts commit a3cb07f35c9c497a99b3891988c767f6d2cffb3e.

--- ProtectTheOrb/Assets/Scripts/Gameplay/GameEvents/GameEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Game Event", menuName = "Game Event")]
-public class GameEvent : MonoBehaviour
-{
-    private List<GameEventListener> listeners = new List<GameEventListener>();
-}
